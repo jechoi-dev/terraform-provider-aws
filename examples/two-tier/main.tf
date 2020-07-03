847c084a26b1ee9d0fe4cd74e0f24ea05a10cf5a
jechoi-dev <67414957+jechoi-dev@users.noreply.github.com>
2020-07-03 10:49:36 +0900

    Update main.tf
    changed vpc cider

--- examples/two-tier/main.tf
+++ examples/two-tier/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 #dd Create a VPC to launch our instances into
 resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "20.0.0.0/16"
 }
 
 # Create an internet gateway to give our subnet access to the outside world
